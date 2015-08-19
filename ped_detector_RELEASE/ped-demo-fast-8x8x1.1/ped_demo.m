7ceb7df3ef15bcd2588ef16b5bd756367101ae2d
Sang Phan <plsang@nii.ac.jp>
2015-08-19 21:48:05 +0900

    changed to function, changed threshold to 0.1

--- ped_detector_RELEASE/ped-demo-fast-8x8x1.1/ped_demo.m
+++ ped_detector_RELEASE/ped-demo-fast-8x8x1.1/ped_demo.m
@@ -1,22 +1,25 @@
-%% non max supression (similar to hog-detector)
-nmax_param.sw = 0.1;
-nmax_param.sh = 0.1;
-nmax_param.ss = 1.3;
-nmax_param.th = 0.0;
+function ped_demo(image),
+    %% non max supression (similar to hog-detector)
+    nmax_param.sw = 0.1;
+    nmax_param.sh = 0.1;
+    nmax_param.ss = 1.3;
+    nmax_param.th = 0.1;
 
-%% detector is run at this scaleratio with a stride of 8x8
-scaleratio = 2^(1/8);
+    %% detector is run at this scaleratio with a stride of 8x8
+    scaleratio = 2^(1/8);
 
-%% test image
-image = 'pos_test.png';
+    %% test image
+    %image = '5672441155.jpg';%'pos_test.png';
 
-%% load precomputed models
-load approx_models;
-approx_model_hard = approx_models{2}; 
-%two slightly different models (#training) 
+    %% load precomputed models
+    load approx_models;
+    approx_model_hard = approx_models{2}; 
+    %two slightly different models (#training) 
 
-%% add path to fast iksvm prediction code
-addpath ../libsvm-mat-2.84-1-fast.v3/
+    %% add path to fast iksvm prediction code
+    addpath ../libsvm-mat-2.84-1-fast.v3/
 
-%% run the detector 
-[dr,ds] = run_detector(image,approx_model_hard,scaleratio,nmax_param);
+    %% run the detector 
+    [dr,ds] = run_detector(image,approx_model_hard,scaleratio,nmax_param);
+
+end
